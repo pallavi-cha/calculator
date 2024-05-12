14a97a803cd98e0cb99715e614f2b505fc00404b
pallavi <pallavichoudhary525@gmail.com>
2024-05-12 23:44:35 +0530

    add new vue file

---
+++ App.vue
@@ -0,0 +1,42 @@
+<temple>
+    <body>
+    <div class="container">
+    <div class="display">
+    <form action "">
+    <input type="text">
+    </form>
+    </div>
+            <div class="buttons">
+            <div class="row">
+            <div class="col-auto">
+            <button class="btn">1</button>
+            <button class="btn">2</button>
+            <button class="btn">3</button>
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-auto">
+            <button class="btn">1</button>
+            <button class="btn">2</button>
+            <button class="btn">3</button>
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-auto">
+            <button class="btn">4</button>
+            <button class="btn">5</button>
+            <button class="btn">6</button>
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-auto">
+            <button class="btn">7</button>
+            <button class="btn">8</button>
+            <button class="btn">9</button>
+            </div>
+            </div>
+
+</temple>
+    <script>
+    import 
+</script>
